c96254fc2a44100e2fdca64e9b783be4d0afda85
JosueeeC <josuecastillo_m@hotmail.com>
2023-11-02 21:27:39 -0500

    1

---
+++ WebApplication1/Controllers/InvoiceController.cs
@@ -0,0 +1,97 @@
+﻿using Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Servicio;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: InvoiceController
+        public ActionResult Index()
+        {
+           
+
+            BInvoice bInvoice = new BInvoice();
+            List<Invoice> invoiceEntity = bInvoice.Get();
+
+            List<InvoiceModel> invoices = invoiceEntity.Select(x=> new InvoiceModel{
+                Invoice_Id = x.Invoice_id,
+                Igv = (x.Total*18)/100,
+                Total = x.Total
+            }).ToList();
+
+            return View(invoices);
+        }
+
+        // GET: InvoiceController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: InvoiceController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: InvoiceController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InvoiceController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: InvoiceController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: InvoiceController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: InvoiceController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
